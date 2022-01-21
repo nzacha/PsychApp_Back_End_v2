bc1e8d234822217bf3b658f641c4d09691ae8d01
nzacha <zicolas3@gmail.com>
2022-01-21 12:23:17 +0200

    fixed email link

--- src/controllers/user_controller.ts
+++ src/controllers/user_controller.ts
@@ -16,7 +16,7 @@ export async function createUser(request: express.Request, response: express.Res
         const salt = crypto.randomBytes(16).toString('hex');
         const user = await Models.User.create({...rest, email: email, password_hash: crypto.pbkdf2Sync(password, salt, 1000, 16, `sha512`).toString(`hex`), password_salt: salt});
         if(user){
-            sendMail(email, 'PsychApp Registration', undefined, `<h1>Registration to PsychApp</h1><h2>Welcome ${user.first_name} ${user.last_name}!</h2><p>Your password is: <b>${password}</b></p><p>use the following link to acces the page: <a>${process.env.WEB_HOST}</a></p>`);
+            sendMail(email, 'PsychApp Registration', undefined, `<h1>Registration to PsychApp</h1><h2>Welcome ${user.first_name} ${user.last_name}!</h2><p>Your password is: <b>${password}</b></p><p>use the following link to acces the page: <a href="${process.env.WEB_HOST}">${process.env.WEB_HOST}</a></p>`);
             const retVal = await Models.User.findOne({where: {user_id: user.user_id}, attributes: {exclude: ['password_hash', 'password_salt', 'tolen']}})
             response.status(200).json(newResponse(retVal, 'New User Created Successfully'));
         }else
